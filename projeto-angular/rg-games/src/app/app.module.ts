8e8f52ea00bfaae1de10b7abe74e57d5d0bb4bc2
Nwldo <regenildoliveira@yahoo.com.br>
2023-12-18 15:28:45 -0300

    Site da loja de jogos no Angular - concluído

--- projeto-angular/rg-games/src/app/app.module.ts
+++ projeto-angular/rg-games/src/app/app.module.ts
@@ -14,16 +14,25 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    LoginComponent,
+    MenuComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatCardModule,
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
